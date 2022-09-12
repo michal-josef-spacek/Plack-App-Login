330e7d51233a95afab44126aa5f123710f6a0124
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-09-12 18:52:44 +0200

    Prepare for 0.06 version

--- Login.pm
+++ Login.pm
@@ -14,7 +14,7 @@ use Tags::HTML::Page::End;
 use Tags::Output::Raw;
 use Unicode::UTF8 qw(decode_utf8 encode_utf8);
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 sub call {
 	my ($self, $env) = @_;
@@ -287,6 +287,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
